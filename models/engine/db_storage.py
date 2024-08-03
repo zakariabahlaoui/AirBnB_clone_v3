1d7fec27ade8c62dc78dd90c5d1bad29c81dea2b
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2024-08-03 01:36:53 +0100

    add method 'def count()'

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -64,12 +64,6 @@ class DBStorage:
         if obj is not None:
             self.__session.delete(obj)
 
-    def get(self, cls, id):
-        """Retrieves one object by its ID."""
-        if cls in self.classes and id and isinstance(id, str):
-            all_objects = self.all(cls)
-            return all_objects.get(id)  # Use dictionary lookup for efficiency
-        return None        
 
     def reload(self):
         """reloads data from the database"""
@@ -81,3 +75,15 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Retrieves one object by its ID."""
+        if cls in self.classes and id and isinstance(id, str):
+            all_objects = self.all(cls)
+            return all_objects.get(id)  # Use dictionary lookup for efficiency
+        return None\
+        
+    def count(self, cls=None):
+        """Counts the number of objects in storage."""
+        objects = self.all(cls) if cls in self.classes else self.all()
+        return len(objects)               
