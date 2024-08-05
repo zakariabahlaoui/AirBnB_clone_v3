1a78e1b9454e550e23e8f63a8f8fa991b6a38e3e
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2024-08-05 17:02:21 +0100

    fix: storage itemes in index file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -27,11 +27,13 @@ def count_each_obj():
         JSON response with the count of each object type.
     """
     obj_counter = {
-        "amenities": storage.count(Amenity),
-        "cities": storage.count(City),
-        "places": storage.count(Place),
-        "reviews": storage.count(Review),
-        "states": storage.count(State),
-        "users": storage.count(User),
+        "amenities": Amenity,
+        "cities": City,
+        "places": Place,
+        "reviews": Review,
+        "states": State,
+        "users": User,
     }
-    return jsonify(obj_counter)
+    dic = {k: storage.count(v) for k, v in obj_counter.items()}
+
+    return jsonify(dic)
