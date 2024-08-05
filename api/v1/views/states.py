9f5726a878d4633479a1ae9f6927f3c106fd9483
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2024-08-05 18:42:08 +0100

    up crreat_state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -58,7 +58,8 @@ def create_state():
     else:
         data_state = State(**data_state)
         data_state.save()
-        
+
+    # return the new state object with CREATED status code 201
     return make_response(jsonify(data_state.to_dict()), 201)
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
@@ -85,4 +86,3 @@ def update_state(state_id):
     storage.save()
 
     return jsonify(state_O.to_dict())
-
