e1ae49842c1966904d5c920c7b90f7c6cb003081
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2024-08-05 17:12:25 +0100

    fix: index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
 """Module to handle API routes related to object counts and status."""
 
-from flask import Blueprint, jsonify
+from api.v1.views import app_views
+from flask import jsonify
 from models import storage
-from models.state import State
-from models.city import City
+
 from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
 from models.place import Place
-from models.user import User
 from models.review import Review
+from models.state import State
+from models.user import User
 
-app_views = Blueprint('app_views', __name__)
 
-
-@app_views.route('/status')
+@app_views.route("/status", strict_slashes=False)
 def api_status():
     """Returns the status of the API."""
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats')
+@app_views.route("/stats", strict_slashes=False)
 def count_each_obj():
     """Retrieves the number of each object by type.
 
