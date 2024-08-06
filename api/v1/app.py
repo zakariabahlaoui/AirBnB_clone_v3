c1222a6bf80e1ef21777674749c62fb4fbf8acf6
zakariabahlaoui <zakariabahlaoui93@gmail.com>
2024-08-06 03:11:17 +0100

    makes a cross-origin HTTP in app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,10 +2,9 @@
 """This file defines the Flask application for the Airbnb clone API."""
 
 from os import getenv
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-from os import environ
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -13,6 +12,7 @@ app = Flask(__name__)
 # Register blueprints
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
 
 @app.teardown_appcontext
 def teardown_app(exception):
